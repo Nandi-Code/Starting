3838bb191338c1553f06f91b8a06400f1049fab2
Nandi18 <sulisnandi@gmail.com>
2020-08-09 14:26:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle
+
+nandi_turtle = turtle.Turtle()
+
+def square():
+  nandi_turtle.forward(100)
+  nandi_turtle.right(90)
+  nandi_turtle.forward(100)
+  nandi_turtle.right(90)
+  nandi_turtle.forward(100)
+  nandi_turtle.right(90)
+  nandi_turtle.forward(100)
+
+square()
+nandi_turtle.forward(200)
+square()
+
+
+
+
